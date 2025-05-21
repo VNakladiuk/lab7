6572a41f0566cfb62b8bb0fbb3846d8e3b7a994c
VNakladiuk <volodymyr.nakladiuk@lnu.edu.ua>
2025-05-21 17:11:03 +0300

    Add files via upload

---
+++ 7.2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Printer {
+private:
+    static Printer* instance;
+
+    Printer() {
+        cout << "[Принтер увімкнено]\n";
+    }
+
+public:
+    Printer(const Printer&) = delete;
+    Printer& operator=(const Printer&) = delete;
+
+    static Printer* getInstance() {
+        if (!instance)
+            instance = new Printer();
+        return instance;
+    }
+
+    void print(string text) {
+        cout << "[Друк]: " << text << endl;
+    }
+};
+
+Printer* Printer::instance = nullptr;
+
+class OfficeWorker {
+private:
+    string name;
+
+public:
+    OfficeWorker(string name) : name(name) {}
+
+    void sendToPrint(string doc) {
+        Printer::getInstance()->print(name + " друкує: " + doc);
+    }
+};
+
+int main() {
+    OfficeWorker alice("Аліса");
+    OfficeWorker bob("Боб");
+
+    alice.sendToPrint("Звіт за березень");
+    bob.sendToPrint("План на квітень");
+
+    return 0;
+}
+
+
